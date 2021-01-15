d24f73dbb7e497483367c0ec14c9e18772ee1fb4
teresaflicek <69260261+teresaflicek@users.noreply.github.com>
2021-01-15 14:28:06 -0600

    fixed charts and errors

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,25 +1,26 @@
 // function for initialization
 function init() {
-
+    
     // read in the samples.json file
     d3.json("samples.json").then((samples) => {
 
         // check if data loaded successfully
-        console.log(samples)
-        console.log(samples.names)
+    //    console.log(samples)
+    //    console.log(samples.names)
 
         // select the dropdown 
         var dropdown = d3.select("#selDataset");
 
         // function for appending the id to the dropdown
         samples.names.forEach(function (name) {
-            console.log(name)
+      //      console.log(name)
             dropdown.append("option").text(name).attr("value", name)
         });
 
         // call funtions for plots and demographic information
-        demoinfo(samples.names);
-        plots(samples.names);
+      
+        demoinfo(samples.names[0]);
+        plots(samples.names[0]);
     });
 };
 
@@ -85,20 +86,20 @@ function plots(name) {
         console.log(sampleValues)
         
         // create variable for otu_ids
-        var otuIds = samps.otu_ids;
+        var otuIds = idSample.otu_ids;
 
         // create variable for otu_labels
-        var labels = samps.otu_labels;
+        var labels = idSample.otu_labels;
 
         // map the otu Id
         var yticks = otuIds.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
 
         // create trace variable for bar plot
         var trace1 = {
-            x: sampleValues.slice(0,10),
-            y: otuIds,
+            x: sampleValues.slice(0,10).reverse(),
+            y: yticks,
             type: 'bar',
-            text: yticks,
+            text: labels.slice(0,10).reverse(),
             orientation: 'h'
         };
 
@@ -130,6 +131,7 @@ function plots(name) {
             x: otuIds,
             y: sampleValues,
             text: labels,
+            mode: 'markers',
             marker: {
                 size: sampleValues,
                 color: otuIds
@@ -144,7 +146,8 @@ function plots(name) {
             title: 'Sample OTUs',
             showlegend: false,
             height: 600,
-            width: 600
+            width: 600,
+            margin: {t:30}
         };
 
         // plot the bubble chart
@@ -154,66 +157,7 @@ function plots(name) {
 
 };
 
-
-// BUBBLE CHART
-// var trace1 = {
-//     x: [1, 2, 3, 4],
-//     y: [10, 11, 12, 13],
-//     text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
-//     mode: 'markers',
-//     marker: {
-//         color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)', 'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-//         size: [40, 60, 80, 100]
-//     }
-// };
-
-// var data = [trace1];
-
-// var layout = {
-//     title: 'Bubble Chart Hover Text',
-//     showlegend: false,
-//     height: 600,
-//     width: 600
-// };
-
-// Plotly.newPlot('myDiv', data, layout);
-
-
-// BAR CHART PLOTLY
-// var trace1 = {
-//     x: ['Liam', 'Sophie', 'Jacob', 'Mia', 'William', 'Olivia'],
-//     y: [8.0, 8.0, 12.0, 12.0, 13.0, 20.0],
-//     type: 'bar',
-//     text: ['4.17 below the mean', '4.17 below the mean', '0.17 below the mean', '0.17 below the mean', '0.83 above the mean', '7.83 above the mean'],
-//     marker: {
-//       color: 'rgb(142,124,195)'
-//     }
-//   };
-
-//   var data = [trace1];
-
-//   var layout = {
-//     title: 'Number of Graphs Made this Week',
-//     font:{
-//       family: 'Raleway, sans-serif'
-//     },
-//     showlegend: false,
-//     xaxis: {
-//       tickangle: -45
-//     },
-//     yaxis: {
-//       zeroline: false,
-//       gridwidth: 2
-//     },
-//     bargap :0.05
-//   };
-
-//   Plotly.newPlot('myDiv', data, layout);
-
-
-
-
-
+// call init function
 init();
 
 
